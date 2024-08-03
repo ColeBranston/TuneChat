434ae847a1420604cad871a19349552da9c74f37
Hetul Patel <patehetu239@gmail.com>
2024-08-03 18:21:16 -0400

    chore: Update Appbar with logo images and styling

--- fullstack/app/components/Appbar.tsx
+++ fullstack/app/components/Appbar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import SigninButton from "./SigninButton";
+import logo1 from "../../public/logo1.png";
+import logo2 from "../../public/logo2.png";
+import Image from 'next/image';
 
 const Appbar = () => {
     return (
-        <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-red-100 shadow">
+        <header className="flex justify-between items-center pl-4 pr-4 pu-1 pd-1 bg-gradient-to-b from-white to-red-100 shadow">
+            <Image src={logo1} alt="Logo" width={70} /> 
             <SigninButton />
         </header>
     )
