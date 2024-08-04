438348a36fc8f6f9aea5292d52bfd044b09580dc
Hetul Patel <patehetu239@gmail.com>
2024-08-03 21:10:50 -0400

    Refactor Chat page to improve message display and scrolling

--- fullstack/app/page.tsx
+++ fullstack/app/page.tsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import Link from 'next/link';
+import logo1 from '../public/logo1.png';
+import Imae from 'next/image';
 
-interface User { // change later
+interface User {
   name: string;
   messages: string
 }
@@ -29,10 +31,11 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen bg-gradient-to-r from-red-50 to-red-200 flex-col items-center justify-between p-24">
-      <div className="flex flex-col gap-[10px]">
-        <h1 className="text-5xl font-sans">Welcome to TuneChat</h1>
-        <Link className="text-2xl font-sans" href="/chat">→ Go to Chat Rooms</Link>
-      </div>
+        <div className="flex flex-col items-center gap-[10px]">
+            <h1 className="text-5xl font-sans">Welcome to TuneChat</h1>
+            <Link className="text-2xl font-sans" href="/chat">→ Go to Chat Rooms</Link>
+            <Image src={logo1} alt="Logo" width={100} />
+        </div>
     </main>
   );
 }
